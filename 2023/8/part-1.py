7efba99b619e717943efe21a85d828379b8e8b66
bertrandvidal <vidal.bertrand@gmail.com>
2023-12-08 07:23:29 -0500

    day 8 inputs + parsing

---
+++ 2023/8/part-1.py
@@ -0,0 +1,23 @@
+import sys
+import re
+
+line_regex = re.compile(r'(?P<node>[A-Z]{3}) = \((?P<L>[A-Z]{3}), (?P<R>[A-Z]{3})\)')
+
+with open(sys.argv[1], "r") as input_file:
+    lines = [line.strip("\n") for line in input_file.readlines()]
+
+instructions = [x for x in lines[0]]
+
+nodes = {}
+start_node, end_node = None, None
+
+for line in lines[1:]:
+    match = line_regex.match(line)
+    if match:
+        groups = match.groups()
+        nodes[groups[0]] = {"L": groups[1], "2": groups[2]}
+        if not start_node:
+            start_node = groups[0]
+
+end_node = groups[0]
+
